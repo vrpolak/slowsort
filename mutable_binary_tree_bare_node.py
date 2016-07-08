f8df86d118767671310e5850b50792d51373b9ec
vrpolak <vratko.polak@gmail.com>
2016-07-08 21:11:52 +0200

    Create mutable_binary_tree_bare_node.py

---
+++ mutable_binary_tree_bare_node.py
@@ -0,0 +1,29 @@
+"""Module that defines abstract mutable binary tree node without value"""
+
+# TODO: Make this Abstract Base Class so that users can rely on isinstance.
+
+class MutableBinaryTreeBareNode(object):
+    """Node of binary tree, not carrying a value.
+    Left and right children are either None or also Node.
+    Self is altered regularily to avoid excessive object creation."""
+
+    def __init__(self):
+        """Initialize an childless node."""
+        raise NotImplementedError
+
+    def get_left_child(self):
+        """Return left child or None."""
+        raise NotImplementedError
+
+    def get_right_child(self):
+        """Return right child or None."""
+        raise NotImplementedError
+
+    def swap_left_child(self, node):
+        """Set node (may be None) as new left child, return the previous left child."""
+        raise NotImplementedError
+
+    def swap_right_child(self, node):
+        """Set node (may be None) as new right child, return the previous right child."""
+        raise NotImplementedError
+
