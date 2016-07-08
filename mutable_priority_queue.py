581f1bbe9169068df7b3fa4bc95c022b515f9183
vrpolak <vratko.polak@gmail.com>
2016-07-08 21:09:37 +0200

    Create mutable_priority_queue.py

---
+++ mutable_priority_queue.py
@@ -0,0 +1,24 @@
+"""Module that defines abstract mutable priority queue"""
+
+# TODO: Make this Abstract Base Class so that users can rely on isinstance.
+
+class MutablePriorityQueue(object):
+    """Priority queue, least priority value in, first out.
+    Self is altered regularily to avoid excessive object creation."""
+
+    def __init__(self):
+        """Initialize an empty queue."""
+        raise NotImplementedError
+
+    def is_empty(self):
+        """Return boolean corresponding to emptiness of the queue."""
+        raise NotImplementedError
+
+    def add(self, item, priority):
+        """Add item to self, return None."""
+        raise NotImplementedError
+
+    def pop(self):
+        """If not empty, extract the least item from self and return that."""
+        raise NotImplementedError
+
