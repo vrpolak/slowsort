e57aca6b73b04266ee28ec0b4762ae0fde2ce07d
Vratko Polak <vratko.polak@gmail.com>
2016-07-09 15:56:08 +0200

    Create default_impl_mutable_binary_tree_laden_node.py

--- default_impl_mutable_binary_tree_laden_node.py
+++ default_impl_mutable_binary_tree_laden_node.py
@@ -1,36 +1,44 @@
-"""Module that defines abstract mutable binary tree node which carries a payload."""
+"""Module that provides the default implementation of MutableBinaryTreeLadenNode."""
 
-# TODO: Make this Abstract Base Class so that users can rely on isinstance.
+from mutable_binary_tree_laden_node import MutableBinaryTreeLadenNode
 
-class MutableBinaryTreeLadenNode(object):
+class DefaultImplMutableBinaryTreeLadenNode(MutableBinaryTreeLadenNode):
     """Node of binary tree, carrying a payload object.
     Left and right children are either None or also Node.
     Self is altered regularily to avoid excessive object creation."""
 
     def __init__(self, payload):
         """Initialize an childless node."""
-        raise NotImplementedError
+        self.payload = payload
+        self.left_child = None
+        self.right_child = None
 
     def get_payload(self):
         """Return the payload, do not change state."""
-        raise NotImplementedError
+        return self.payload
 
     def get_left_child(self):
         """Return left child or None, do not change state."""
-        raise NotImplementedError
+        return self.left_child
 
     def get_right_child(self):
         """Return right child or None, do not change state."""
-        raise NotImplementedError
+        return self.right_child
 
     def swap_payload(self, payload):
         """Set the new payload, return the old payload."""
-        raise NotImplementedError
+        odl_payload = self.payload
+        self.payload = payload
+        return odl_payload
 
     def swap_left_child(self, node):
         """Set node (may be None) as new left child, return the previous left child."""
-        raise NotImplementedError
+        old_left_child = self.left_child
+        self.left_child = node
+        return odl_left_child
 
     def swap_right_child(self, node):
         """Set node (may be None) as new right child, return the previous right child."""
-        raise NotImplementedError
+        old_right_child = self.right_child
+        self.right_child = node
+        return odl_right_child
